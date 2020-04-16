149ab6f03c3345d30806652d7043410e4e017fc1
Hossam Mohamed <a01122882174@gmail.com>
2020-04-16 10:45:23 +0200

    Data Base Updated Again
    Added Tables typeLinks,Links to the Erd And The Db on phpmyadmin
    
    it will be just exist but it will never use
    
    (instructor asked me to add them but not use it in case our project )

--- DB/Online_Order_Food.sql
+++ DB/Online_Order_Food.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2020 at 09:43 AM
+-- Generation Time: Apr 16, 2020 at 10:42 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,6 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `links`
+--
+
+CREATE TABLE `links` (
+  `ID` int(11) NOT NULL,
+  `Link_Name` varchar(30) NOT NULL,
+  `Url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `Order_Id` int(11) NOT NULL,
+  `Order_Desc` varchar(100) NOT NULL,
+  `Order_Date` datetime NOT NULL DEFAULT current_timestamp(),
+  `Order_Statue` varchar(15) NOT NULL DEFAULT 'Not Delivered',
+  `Total_Cost` float NOT NULL,
+  `Cust_Id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -36,9 +63,76 @@ CREATE TABLE `products` (
   `Pro_Img` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_links`
+--
+
+CREATE TABLE `type_links` (
+  `UserTypeId` int(11) NOT NULL,
+  `Link_Id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(10) NOT NULL,
+  `User_Name` varchar(10) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `Phone` varchar(11) DEFAULT NULL,
+  `Address` varchar(100) NOT NULL,
+  `User_Type_Id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Id`, `Name`, `User_Name`, `Password`, `Phone`, `Address`, `User_Type_Id`) VALUES
+(1, 'Hossam', 'Admin', '123', '01156826636', 'Nasr City , Cairo', 1),
+(2, 'User', 'user', '123', '01156826636', 'Helwan , Cairo ', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_type`
+--
+
+CREATE TABLE `user_type` (
+  `ID` int(11) NOT NULL,
+  `Type_Name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_type`
+--
+
+INSERT INTO `user_type` (`ID`, `Type_Name`) VALUES
+(1, 'ADMIN'),
+(2, 'USER');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`Order_Id`),
+  ADD KEY `Cust_Id` (`Cust_Id`);
 
 --
 -- Indexes for table `products`
@@ -47,14 +141,71 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`Pro_Id`);
 
 --
+-- Indexes for table `type_links`
+--
+ALTER TABLE `type_links`
+  ADD PRIMARY KEY (`UserTypeId`),
+  ADD KEY `Link_Id` (`Link_Id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `User_Name` (`User_Name`),
+  ADD KEY `Foreign key` (`User_Type_Id`);
+
+--
+-- Indexes for table `user_type`
+--
+ALTER TABLE `user_type`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Type_Name` (`Type_Name`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `Order_Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `Pro_Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`Cust_Id`) REFERENCES `users` (`Id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `type_links`
+--
+ALTER TABLE `type_links`
+  ADD CONSTRAINT `type_links_ibfk_1` FOREIGN KEY (`UserTypeId`) REFERENCES `user_type` (`ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `type_links_ibfk_2` FOREIGN KEY (`Link_Id`) REFERENCES `links` (`ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `Foreign key` FOREIGN KEY (`User_Type_Id`) REFERENCES `user_type` (`ID`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
