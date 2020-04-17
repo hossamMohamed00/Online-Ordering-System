258ea9a198740081e3b475b5a0c873873f764518
Hossam Mohamed <a01122882174@gmail.com>
2020-04-17 14:31:32 +0200

    Update Online_Order_Food.sql
    Database structure updated
    change datatype of statue column in table order To be Enum has only 2 values (Delivered , Not Delivered Yet)
    
    -cahnge the data type of pro_Desc To be Text alternate of varchar

--- DB/Online_Order_Food.sql
+++ DB/Online_Order_Food.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2020 at 07:05 PM
+-- Generation Time: Apr 17, 2020 at 02:26 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,32 +25,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_type`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user_type` (
-  `ID` int(11) NOT NULL,
-  `Type_Name` varchar(30) NOT NULL
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(10) NOT NULL,
+  `User_Name` varchar(10) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  `Phone` varchar(11) DEFAULT NULL,
+  `Address` text NOT NULL,
+  `User_Type_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_type`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `user_type` (`ID`, `Type_Name`) VALUES
-(1, 'ADMIN'),
-(2, 'USER');
+INSERT INTO `users` (`Id`, `Name`, `User_Name`, `Password`, `Phone`, `Address`, `User_Type_Id`) VALUES
+(1, 'Hossam', 'Admin', '123', '01156826636', 'Nasr City , Cairo', 1),
+(2, 'User', 'user', '123', '01156826636', 'Helwan , Cairo ', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user_type`
+-- Indexes for table `users`
 --
-ALTER TABLE `user_type`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Type_Name` (`Type_Name`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `User_Name` (`User_Name`),
+  ADD KEY `Foreign key` (`User_Type_Id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `Foreign key` FOREIGN KEY (`User_Type_Id`) REFERENCES `user_type` (`ID`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
