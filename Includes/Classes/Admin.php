32421d08114cad0a3b198819d09c3a50370255ef
Hossam Mohamed <a01122882174@gmail.com>
2020-05-14 23:36:23 +0200

    Add Generate Functions

--- Includes/Classes/Admin.php
+++ Includes/Classes/Admin.php
@@ -1,4 +1,5 @@
 <?php
+require_once('tcpdf_min/tcpdf.php'); 
 class Admin extends person 
 {
     
@@ -233,4 +234,32 @@ class Admin extends person
     {
         return $this->order->Filter_Orders($from_date , $to_date);
     }
+
+    /**
+     * Generate Report of Orders History and the taotal profit
+     * @param String The content which will be in the PDF
+     */
+    public function Generate_Report($content)
+    {
+        //Prepare Pdf
+        $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
+        $obj_pdf->SetCreator(PDF_CREATOR);  
+        $obj_pdf->SetTitle("Orders Report");
+        $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
+        $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
+        $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
+        $obj_pdf->SetDefaultMonospacedFont('helvetica');  
+        $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '5', PDF_MARGIN_RIGHT);  
+        $obj_pdf->setPrintHeader(false);  
+        $obj_pdf->setPrintFooter(false);  
+        $obj_pdf->SetAutoPageBreak(TRUE, 10);  
+        $obj_pdf->SetFont('helvetica', '', 10);  
+        $obj_pdf->AddPage();
+
+
+
+        $obj_pdf->writeHTML($content);  
+        $obj_pdf->Output('Orders_Report.pdf', 'I');
+    }
+
 }
--- admin/Generate_Pdf.php
+++ admin/Generate_Pdf.php
@@ -8,29 +8,13 @@
   */
 
 
-  include 'init.php';
-  require_once('tcpdf_min/tcpdf.php');  
+  include 'init.php'; 
   session_start();
 
 
 if(isset($_POST["create_pdf"]))  
  {  
-
-      //Prepare Pdf
-      $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);  
-      $obj_pdf->SetCreator(PDF_CREATOR);  
-      $obj_pdf->SetTitle("Orders Report");
-      $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);  
-      $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));  
-      $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));  
-      $obj_pdf->SetDefaultMonospacedFont('helvetica');  
-      $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '5', PDF_MARGIN_RIGHT);  
-      $obj_pdf->setPrintHeader(false);  
-      $obj_pdf->setPrintFooter(false);  
-      $obj_pdf->SetAutoPageBreak(TRUE, 10);  
-      $obj_pdf->SetFont('helvetica', '', 10);  
-      $obj_pdf->AddPage();
-        
+    //hold the Pdf content  on it 
       $content = '';  
 
       //get the data 
@@ -165,6 +149,7 @@ if(isset($_POST["create_pdf"]))
       </tbody> 
         </table>';  
 
-      $obj_pdf->writeHTML($content);  
-      $obj_pdf->Output('Orders_Report.pdf', 'I');
+
+        //call the function to genereate the pdf 
+        $admin->Generate_Report($content);
  }
